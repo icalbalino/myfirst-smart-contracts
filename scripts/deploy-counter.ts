ce462c12363b22a4a5725085dd03165143a1ee6c
icalbalino <balino.ical@gmail.com>
2022-07-25 03:51:32 +0700

    create counter contract and deploy script

---
+++ scripts/deploy-counter.ts
@@ -0,0 +1,10 @@
+import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+
+async function deploy() {
+    const C = await ethers.getContractFactory("Counter");
+    const counter = await C.deploy();
+    console.log(await counter.count());
+}
+
+deploy();
