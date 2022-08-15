654cf0fdc957dd6c01973150e09dcfe2b6f08048
icalbalino <balino.ical@gmail.com>
2022-08-15 23:00:41 +0700

    diamond pattern calls

--- scripts/deploy-fallback.ts
+++ scripts/deploy-fallback.ts
@@ -1,9 +1,9 @@
 import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
 
-async function deploy() {
-    const Fallback = await ethers.getContractFactory("Fallback");
-    const fallback = await Fallback.deploy();
+async function deploy(name, ...args) {
+    const Fallback = await ethers.getContractFactory(name);
+    const fallback = await Fallback.deploy(...args);
     await fallback.deployed();
     
     console.log(fallback.address);
@@ -11,10 +11,44 @@ async function deploy() {
     return fallback;
 }
 
-async function fallback(fallback) {
-    const f = await ethers.getContractAt("IFallback", fallback.address);
-    await f.count();
+// async function fallback(fallback) {
+//     const f = await ethers.getContractAt("IFallback", fallback.address);
+//     await f.count();
+// }
+
+async function fallback() {
+    const a = await deploy("A");
+    const b = await deploy("B", a.address);
+
+    console.log("A", await a.getA());
+    console.log("B", await b.getB());
+    console.log("------------");
+
+    await a.setA(41);
+    console.log("A", await a.getA());
+    console.log("B", await b.getB());
+    console.log("------------");
+
+    await b.setB(60);
+    console.log("A", await a.getA());
+    console.log("B", await b.getB());
+    console.log("------------");
 }
 
-deploy().then(fallback);
+fallback();
+//deploy().then(fallback);
 // deploy();
+
+
+// class Foo {
+//     private bar: number;
+//     foo() {
+//         console.log("foo", this.bar);
+//     }
+// }
+
+// const foo = new Foo();
+// foo.foo();
+// foo.foo.call({
+//     bar: 42
+// })
